a1e570f1d930d75c83b227f8315f4dfcb1b637db
GTrunSec <gtrunsec@hardenedlinux.org>
2022-07-02 00:14:35 -0700

    Update

--- cells/zeek/configFiles/default.nix
+++ cells/zeek/configFiles/default.nix
@@ -5,5 +5,5 @@
   inherit (inputs) nixpkgs;
 in {
   mkNode = import ./mkNode.nix args;
-  pre-zeekctl = ./pre-zeekctl.bash;
+  pre-zeekctl = nixpkgs.writers.writeBash "pre-zeekctl.bash" (builtins.readFile ./pre-zeekctl.bash);
 }
